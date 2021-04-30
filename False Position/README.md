ce5125664f3b2c85c4a5151f199bb876e7c9acfc
k0besity <83366176+k0besity@users.noreply.github.com>
2021-04-30 10:53:29 -0600

    Added false position algorithm.

---
+++ False Position/README.md
@@ -0,0 +1,6 @@
+False Position
+==============
+
+This algorithm uses the root finding technique of false position. The
+required inputs include a function, and then a lower and upper estimate
+of the root.
---
+++ False Position/falsePosition.m
@@ -0,0 +1,36 @@
+function [root, fx, ea, iter] = falsePosition(func, xl, xu, es, maxit, varargin)
+%falsePosition finds the root of a function using false position method
+
+if nargin < 3
+    error('not enough inputs');
+end
+
+test = func(xl,varargin{:}) * func(xu,varargin{:});
+if test>0
+    error('no sign change')
+end
+if nargin<4||isempty(es), es = 0.0001;end
+if nargin<5||isempty(maxit), maxit = 200;end
+iter = 0; xr = xl; ea = 100;
+
+while(1)
+    xrold = xr;
+    xr = (-func(xu) * (xu - xl))/(func(xu) - func(xl)) + xu;
+    iter = iter + 1;
+    if xr ~= 0
+        ea = abs((xr - xrold)/xr) * 100;
+    end
+    test = func(xl,varargin{:}) * func(xr,varargin{:});
+  if test < 0
+    xu = xr;
+  elseif test > 0
+    xl = xr;
+  else
+    ea = 0;
+  end
+  if ea <= es || iter >= maxit,break,end
+end
+root = xr; fx = func(xr, varargin{:});
+
+
+end
